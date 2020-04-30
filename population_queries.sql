6b74ccfb183e3fedb91b3c70cf2b2418fab0ee6c
jwelly <64269613+jwelly@users.noreply.github.com>
2020-04-30 16:04:03 +0100

    Updated ON positionings

--- population_queries.sql
+++ population_queries.sql
@@ -9,7 +9,7 @@ WHERE continent = 'Africa';
 SELECT ROUND(SUM(population), 2) AS 'Population of Oceania in 2005 (in millions)'
 FROM population_years
 JOIN countries
-	ON countries.id = population_years.country_id
+ON countries.id = population_years.country_id
 WHERE continent = 'Oceania' AND year = 2005;
 
 -- What is the average population of countries in South America in 2003?
@@ -17,7 +17,7 @@ WHERE continent = 'Oceania' AND year = 2005;
 SELECT ROUND(AVG(population_years.population), 2) AS 'Average population of South America in 2003'
 FROM population_years
 JOIN countries
-	ON countries.id = population_years.country_id
+ON countries.id = population_years.country_id
 WHERE countries.continent = 'South America' AND population_years.year = 2003;
 
 -- What country had the smallest population in 2007?
@@ -25,7 +25,7 @@ WHERE countries.continent = 'South America' AND population_years.year = 2003;
 SELECT countries.name AS 'Country with the lowest population in 2007', MIN(population_years.population) AS 'Population (in millions)'
 FROM population_years
 JOIN countries
-	ON countries.id = population_years.country_id
+ON countries.id = population_years.country_id
 WHERE population_years.year = 2007;
 
 -- What is the average population of Poland during the time period covered by this dataset?
@@ -33,7 +33,7 @@ WHERE population_years.year = 2007;
 SELECT countries.name AS 'Country', ROUND(AVG(population_years.population), 2) AS 'Average population between 2000 and 2010 (in millions)'
 FROM population_years
 JOIN countries
-	ON population_years.country_id = countries.id
+ON population_years.country_id = countries.id
 WHERE countries.name = 'Poland';
 
 -- How many countries have the word "The" in their name?
@@ -47,7 +47,7 @@ WHERE name LIKE '% The%';
 SELECT countries.continent AS 'Continent', ROUND(SUM(population_years.population), 2) AS 'Total population in 2010'
 FROM countries
 JOIN population_years
-	ON countries.id = population_years.country_id
+ON countries.id = population_years.country_id
 WHERE population_years.year = 2010
 GROUP BY 1
 ORDER BY 2 DESC;
